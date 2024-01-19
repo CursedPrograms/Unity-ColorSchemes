398e685547d8038ea999522d009f98adcf1851e3
Cursed Entertainment <cursedsoft@gmail.com>
2024-01-19 18:35:17 +0200

    First

---
+++ Assets/Action/ActionColors00.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class ActionColors00 : MonoBehaviour
+{
+    Color ColorScheme_Color00 = new Color(255f / 255f, 130f / 255f, 0f);
+    Color ColorScheme_Color01 = new Color(4f / 255f, 30f / 255f, 66f / 255f);
+    Color ColorScheme_Color02 = new Color(127f / 255f, 106f / 255f, 83f / 255f);
+    Color ColorScheme_Color03 = new Color(198f / 255f, 189f / 255f, 180f / 255f);
+    Color ColorScheme_Color04 = new Color(238f / 255f, 234f / 255f, 229f / 255f);
+}
